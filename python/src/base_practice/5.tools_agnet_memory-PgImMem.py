8e3986a0b048e3e57a22cd042eab0d9c3a5f6438
閺傜懓娼³ <fangk046@tydic.com>
2026-07-31 14:50:50 +0800

    添加pg 消息保存-历史消息查看

---
+++ python/src/base_practice/5.tools_agnet_memory-PgImMem.py
@@ -0,0 +1,57 @@
+# agent tools 调用
+from langchain.agents import create_agent
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.postgres import PostgresSaver
+
+load_dotenv()
+
+import os
+
+model = ChatOpenAI(
+    model=os.environ.get("OPENAI_MODEL", "qwen3.5-plus"),
+    base_url=os.environ.get("OPENAI_BASE_URL"),
+    api_key=os.environ.get("OPENAI_API_KEY"),
+    temperature=float(os.environ.get("OPENAI_TEMPERATURE", "0.7")),
+    max_tokens=int(os.environ.get("OPENAI_MAX_TOKENS", "2000")),
+)
+db_url = os.environ.get("DATABASE_URI")
+
+with PostgresSaver.from_conn_string(db_url) as postgresSaver:
+    # postgresSaver.setUp()
+    agent = create_agent(
+        model=model,
+        checkpointer=postgresSaver
+    )
+
+    print(f'agent = {agent}')
+    print(f'agent.nodes = {agent.nodes}')
+    print("============第一轮============")
+
+    config = {"configurable": {"thread_id": "1"}}
+    # 第一轮问答
+    results = agent.invoke(
+        {
+            "messages": [
+                {"role": "user", "content": "来一首送词"}
+            ]
+        },
+        config=config
+    )
+    msgs = results["messages"]
+    for msg in msgs:
+        msg.pretty_print()
+
+
+    # 第二轮问答
+    print("============第二轮============")
+    results = agent.invoke(
+        {
+            "messages": {"role": "user", "content": "再来一首"}
+        },
+        config=config
+    )
+
+    msgs = results["messages"]
+    for msg in msgs:
+        msg.pretty_print()
